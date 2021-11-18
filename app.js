ccf09b8906817b0ce302b542dac6334d9dbd90b8
Gabrielle Dwek <grd274@nyu.edu>
2021-11-18 06:33:27 -0500

    testing

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.get('/manage', (req, res) => {
 });
 
 app.get('/view', (req, res) => {
+  console.log('hi');
   console.log(req.session.username);
   User.findOne({username: req.session.username}, (err, user) => {
     if(err){
@@ -96,6 +97,9 @@ app.get('/view', (req, res) => {
         });
       });
       res.render('view', {'outfits': outfits_local});
+    }
+    else if (!err && !user){
+      console.log('hi');
     }
     else {
       return res.render('error', {'message' : 'user does not exist'});
