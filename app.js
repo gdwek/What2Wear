8e4d06f8dc170d1f4a6847e895bc651f67131912
Gabrielle Dwek <grd274@nyu.edu>
2021-11-28 22:31:28 -0500

    testing api json conversion

--- app.js
+++ app.js
@@ -152,10 +152,9 @@ app.post('/login', (req, res) => {
                           const weatherURL = "http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=QFHUQmXwDaHJ1lqAlP4CTtDATkFA8RcG&q=" + user.zipcode;
                           console.log(weatherURL);
                           request(weatherURL, function(error, response, body) {
-                                console.log(body);
                                 let weather_json = JSON.parse(body);
-                                const weather =  weather_json.Key;
-                                console.log(weather_json);
+                                const weather =  weather_json[0].Key;
+                                console.log(weather);
                           });
                           return res.redirect('/');
                       } 
