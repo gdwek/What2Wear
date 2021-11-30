87db6b93b621e5a785929a9d697637629e190c4e
Gabrielle Dwek <grd274@nyu.edu>
2021-11-30 11:09:23 -0500

    passport implementation

--- app.js
+++ app.js
@@ -60,22 +60,22 @@ app.use(flash());
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { 
-        return done(err); 
-      }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.verifyPassword(password)) { 
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function (err, user) {
+//       if (err) { 
+//         return done(err); 
+//       }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.verifyPassword(password)) { 
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
 
 app.get('/', (req, res) => {
   if(req.session.username){
