d3fa8cd2c9c5b684a0e498d4c9cd46c93b36d7f1
Gabrielle Dwek <grd274@nyu.edu>
2021-12-01 19:16:12 -0500

    added mocha tests

--- test/test.js
+++ test/test.js
@@ -24,7 +24,7 @@ const sinon = require("sinon");
     });
 });
 
-describe ('login', function() {
+describe ('signup', function() {
     it('status', function(done){
         request('https://enigmatic-wave-01914.herokuapp.com/signup', function(error, response, body) {
             expect(response.statusCode).to.equal(200);
@@ -35,40 +35,11 @@ describe ('login', function() {
 
 describe ('accessing view without logging in', function() {
     it('status', function(done){
-        request('https://enigmatic-wave-01914.herokuapp.com/view', function(error, response, body) {
-            expect(response.statusCode).to.equal(503);
+        request('https://enigmatic-wave-01914.herokuapp.com/zipcode', function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
             done();
         });
     });
 });
 
-  // describe("app", function() {
-  //   const stubValue = {
-  //     name: faker.name.findName(),
-  //     password: faker.internet.password(),
-  //     zipcode: faker.address.zipCode(),
-  //   };
-  //   describe("signup", function() {
-  //     it("should add a new user to the db", async function() {
-  //       const stub = sinon.stub(UserModel, "create").returns(stubValue);
-  //       const userRepository = new UserRepository();
-  //       const user = await userRepository.create(stubValue.name, stubValue.email);
-  //       expect(stub.calledOnce).to.be.true;
-  //       expect(user.id).to.equal(stubValue.id);
-  //       expect(user.name).to.equal(stubValue.name);
-  //       expect(user.email).to.equal(stubValue.email);
-  //       expect(user.createdAt).to.equal(stubValue.createdAt);
-  //       expect(user.updatedAt).to.equal(stubValue.updatedAt);
-  //     });
-  //   });
-  // });
-
-  // describe ('About page', function() {
-  //     it('status', function(done){
-  //         request('http://localhost:8080/about', function(error, response, body) {
-  //             expect(response.statusCode).to.equal(404);
-  //             done();
-  //         });
-  //     });
-
-  // });
+  
